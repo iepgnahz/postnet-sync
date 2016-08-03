86ac73c5ee49fc948e6b14ecf2aee4a616b44b09
zhangpei <13488233237@163.com>
2016-08-03 19:26:38 +0800

    finish all

--- client/core.js
+++ client/core.js
@@ -12,17 +12,13 @@ class BarcodeAction {
     this.help = "请输入条形码或者q退出:";
   }
 
-  doAction(cmd) {
+  doAction(cmd,transform,output) {
     if (cmd === "q") {
-      return "init";
+      let current =  "init";
+      transform(current,output);
     } else {
-      // let postcode = changeBarcode(cmd);
-      // if (postcode === "您输入的条形码中有不存在的码型" || barcode === "您输入条形码的校验值和真实校验值不符" || barcode === "您输入的条形码不合法") {
-      //   return "barcode";
-      // } else {
-      //   return "init";
-      // }
-      return changePostcodeAction(cmd.trim())
+      let current = "init";
+      changeBarcodeAction(cmd,transform,current,output)
 
     }
   }
@@ -37,7 +33,8 @@ class PostcodeAction {
 
   doAction(cmd,transform,output) {
     if (cmd === "q") {
-      return "init";
+      let current =  "init";
+      transform(current,output);
     } else {
      let current = "init";
       changePostcodeAction(cmd,transform,current,output);
@@ -59,7 +56,8 @@ class InitAction {
         transform(current,output);
         break;
       case "2":
-        return "barcode";
+        current = "barcode";
+        transform(current,output);
         break;
       case "q":
         console.log("GoodBye");
@@ -87,70 +85,36 @@ function start(repl) {
   console.log(routerSwitcher.start());
   repl.start({
     prompt: '> ', eval: function(cmd, context, filename, output) {
-      // if ((routerSwitcher.currentStatus === "postcode" || routerSwitcher.currentStatus === "barcode" ) && cmd.trim() !== "q") {
-      //   changeCode(cmd, output, routerSwitcher.currentStatus);
-      // } else {
-      //   let newStatus = routerSwitcher.switchRouter(cmd.trim());
-      //   output(newStatus.help);
-      // }
-
       routerSwitcher.switchRouter(cmd.trim(), output);
     }
   });
 }
 
 
-function changePostcode(cmd) {
+start(repl);
+
+function changePostcodeAction(cmd,transform,current,output) {
   var changedCode = "";
   request
-  // .post("http://192.168.1.170:3000/zipcode")
     .post(`http://127.0.0.1:5000/postcode`)
     .type("form")
     .send({code: cmd.trim()})
     .end(function (err, res) {
       changedCode = res.text;
       console.log(res.text);
-      // let newStatus = routerSwitcher.switchRouter(cmd.trim());
-      // output(newStatus.help);
+      transform(current,output);
     });
-  return changedCode
 }
 
-function changeBarcode(cmd) {
+function changeBarcodeAction(cmd,transform,current,output) {
   var changedCode = "";
   request
-  // .post("http://192.168.1.170:3000/zipcode")
     .post(`http://127.0.0.1:5000/barcode`)
     .type("form")
     .send({code: cmd.trim()})
     .end(function (err, res) {
       changedCode = res.text;
       console.log(res.text);
-      // let newStatus = routerSwitcher.switchRouter(cmd.trim());
-      // output(newStatus.help);
-    });
-  return changedCode
-}
-start(repl);
-
-function changePostcodeAction(cmd,transform,current,output) {
-  var changedCode = "";
-  var status = "";
-  request
-  // .post("http://192.168.1.170:3000/zipcode")
-    .post(`http://127.0.0.1:5000/postcode`)
-    .type("form")
-    .send({code: cmd.trim()})
-    .end(function (err, res) {
-      changedCode = res.text;
-      console.log(res.text);
-      // status = (changedCode === "您输入的邮编不合法,应是5,9或者10数字0-9字符,并且若有-,其应在第六位处,请重新输入") ? "postcode" : "barcode"
-      // // let newStatus = routerSwitcher.switchRouter(cmd.trim());
-      // // output(newStatus.help);
-      // status =
       transform(current,output);
-
     });
-  return status
-
 }
--- sever/ChangedPostcode.js
+++ sever/ChangedPostcode.js
@@ -61,7 +61,7 @@ class ChangedPostcode {
       let barcodes = changeCode(checkCode, postCode, items);
       return addFrame(barcodes);
     }
-    return "您输入的邮编不合法,应是5,9或者10数字0-9字符,并且若有-,其应在第六位处,请重新输入";
+    return "您输入的邮编不合法,应是5,9或者10数字0-9字符,并且若有-,其应在第六位处";
   }
 }
 
