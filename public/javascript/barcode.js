73a323dc8e85a357a3f2f79821eefd73195a8632
xin-guo <xinguo@thoughtworks.com>
2016-08-04 15:17:04 +1000

    Barcode page logic finish

--- public/javascript/barcode.js
+++ public/javascript/barcode.js
@@ -5,20 +5,10 @@ $(document).ready(() => {
   $('#change').click(() => {
     $.post(
       "http://127.0.0.1:5000/barcode",
-      { bankCardNumber: localStorage.getItem("logedUser"), amount: $('#amount').val() },
+      { code: $('#code').val() },
       (data) => {
-        if (data.error) {
-          alert(data.error);
-        } else {
-          let record = {
-            type: "存款",
-            amount: $('#amount').val(),
-            cardNumber: localStorage.getItem("logedUser"),
-            date: Date()
-          };
-          localStorage.setItem('currentRecord', JSON.stringify(record));
-          window.location.href = "http://127.0.0.1:8080/record";
-        }
+        console.log(data);
+        $('#result').html(data);
       }
     )
   });
--- public/javascript/init.js
+++ public/javascript/init.js
@@ -2,11 +2,12 @@
  * Created by zhangpei on 16/8/4.
  */
 $(document).ready(() => {
+  console.log(111);
   menuBtnsAction();
 });
 
 function menuBtnsAction() {
-  $('#postcode').click(() => { window.location.href = "http://127.0.0.1:5000/postcode"; });
-  $('#barcode').click(() => { window.location.href = "http://127.0.0.1:5000/barcdoe"; });
+  $('#postcode').click(() => { window.location.href = "http://127.0.0.1:5000/html/postcode.html"; });
+  $('#barcode').click(() => { window.location.href = "http://127.0.0.1:5000/html/barcode.html"; });
   $('#return').click(() => { window.location.href = "http://127.0.0.1:5000/html/init.html"; });
 }
